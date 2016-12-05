6d211d1f8901cd9d5514e5352fef47855bec2338
R Nims <rleenims@gmail.com>
2016-12-05 23:57:18 +0000

    corrected some bad values and truncated decimals for expected results

--- temperature_test.php
+++ temperature_test.php
@@ -1,14 +1,14 @@
 <?php
 //calculator_test2.php
 require_once 'simpletest/autorun.php';
-include 'Temperature.php';
+include 'temperatureclass.php';
 
 class TestOfTemperature extends UnitTestCase {
-    
+
 	function testCelsiusToKelvin() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->c2k(100), 373.15, "100 Celsius is 373.15 Kelvin");
-	  $this->assertEqual($myTemp->c2k(0), 273.15, "0 Celsius is 273.15 Kelvin");
+	  $this->assertEqual($myTemp->c2k(100), 373, "100 Celsius is 373 Kelvin");
+	  $this->assertEqual($myTemp->c2k(0), 273, "0 Celsius is 373 Kelvin");
 	}
 	function testCelsiusToFahrenheit() {
 	  $myTemp= new Temperature();
@@ -18,8 +18,8 @@ class TestOfTemperature extends UnitTestCase {
     
 	function testFahrenheitToKelvin() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->f2k(212), 373.15, "212 Fahrenheit is 373.15 Kelvin");
-	  $this->assertEqual($myTemp->f2k(32), 273.15, "32 Fahrenheit is 273.15 Kelvin");
+	  $this->assertEqual($myTemp->f2k(212), 373, "212 Fahrenheit is 373 Kelvin");
+	  $this->assertEqual($myTemp->f2k(32), 273, "32 Fahrenheit is 273 Kelvin");
 	}
 	function testFahrenheitToCelsius() {
 	  $myTemp= new Temperature();
@@ -29,13 +29,14 @@ class TestOfTemperature extends UnitTestCase {
     
 	function testKelvinToCelsius() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->k2c(100), 373.15, "100 Kelvin is 373.15 Celsius");
-	  $this->assertEqual($myTemp->k2c(0), -273.15, "0 Kelvin is -273.15 Celsius");
+	  $this->assertEqual($myTemp->k2c(100), -173, "100 Kelvin is =173 Celsius");
+	  $this->assertEqual($myTemp->k2c(0), -273, "0 Kelvin is -273 Celsius");
+	  echo($myTemp->k2c(0));
 	}
 	function testKelvinToFahrenheit() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->k2f( 273.15), 0, "273.15 Kelvin is 0 Fahrenheit");
-	  $this->assertEqual($myTemp->k2f(0), -459.67, "0 Kelvin is -459.67 Fahrenheit");
+	  $this->assertEqual($myTemp->k2f(255), 0, "255 Kelvin is 0 Fahrenheit");
+	  $this->assertEqual($myTemp->k2f(0), -459, "0 Kelvin is -459 Fahrenheit");
 	}
 	
 }
