303307248bce2fbd07e9e125d409feb2af73da6b
Ariton84 <vanver@gmail.com>
2016-12-06 13:48:44 -0800

    Update temperatureclass.php

--- temperatureclass.php
+++ temperatureclass.php
@@ -1,37 +1,30 @@
+
 <?php
-require_once('convert.php');
 
 class Temperature
      {
-        function f2c($degree){
-			  
-			 return ($degree -32)*5/9;
+        function f2c($degree)
+	{
+		return ($degree -32)*5/9;
         }
-     
-        function c2f($degree){
-			 
-             return $degree*9/5+32;
+    	function c2f($degree)
+	{
+	     	return $degree*9/5+32;
+        }        
+        function f2k($degree) 
+	{             
+	        return 5/9*($degree - 32) + 273;
         }
-        
-        function f2k($degree) { 
-            
-             return 5/9*($degree - 32) + 273;
-			 
+        function c2k($degree) 
+	{
+                return $degree + 273;
         }
-        function c2k($degree) {
-            
-            return $degree + 273;
-        }
-        function k2c($degree) {
-            
-
-            return $degree - 273;
-
-        
+        function k2c($degree) 
+	{
+                return $degree - 273;
         }
         function k2f($degree) {
-            
-            return 9/5*($degree - 273) + 32;
+                return 9/5*($degree - 273) + 32;
         }
      }
 ?>
