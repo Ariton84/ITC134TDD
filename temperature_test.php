7041b87ad1fd10f3ac965c0f77cfc37f39c4a35b
Ariton84 <vanver@gmail.com>
2016-12-06 17:47:15 -0800

    Update temperature_test.php

--- temperature_test.php
+++ temperature_test.php
@@ -2,12 +2,11 @@
 //calculator_test2.php
 include_once 'convert.php';
 
-
 class TestTemperature extends PHPUnit_Framework_TestCase {
 
 	function testCelsiusToKelvin() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->c2k(100), 373);
+	  $this->assertEqual($myTemp->c2k,100, 373, "100 Celsius is 373 Kelvin");
 	  $this->assertEqual($myTemp->c2k(0), 273, "0 Celsius is 373 Kelvin");
 	  $this->assertPattern('/error/i', $myTemp->c2k(-274), "-274 Celsius should generate error message");	
 	}
