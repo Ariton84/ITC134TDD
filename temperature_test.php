d2bd08a61a508459e6ca9c47c09c44ccf836c975
R Nims <rleenims@gmail.com>
2016-12-08 22:21:16 +0000

    Unit tests now expect temps with 2 digits of decimal accuracy

--- temperature_test.php
+++ temperature_test.php
@@ -12,10 +12,11 @@ class TestTemperature extends PHPUnit_Framework_TestCase {
                 );
         }
     }
+    
 	function testCelsiusToKelvin() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->c2k(100), 373, "100 Celsius is 373 Kelvin");
-	  $this->assertEqual($myTemp->c2k(0), 273, "0 Celsius is 373 Kelvin");
+	  $this->assertEqual($myTemp->c2k(100), 373.15, "100 Celsius is 373.15 Kelvin");
+	  $this->assertEqual($myTemp->c2k(0), 273.15, "0 Celsius is 273.15 Kelvin");
 	  $this->assertPattern('/error/i', $myTemp->c2k(-274), "-274 Celsius should generate error message");	
 	}
 	function testCelsiusToFahrenheit() {
@@ -27,8 +28,8 @@ class TestTemperature extends PHPUnit_Framework_TestCase {
     
 	function testFahrenheitToKelvin() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->f2k(212), 373, "212 Fahrenheit is 373 Kelvin");
-	  $this->assertEqual($myTemp->f2k(32), 273, "32 Fahrenheit is 273 Kelvin");
+	  $this->assertEqual($myTemp->f2k(212), 373.15, "212 Fahrenheit is 373.15 Kelvin");
+	  $this->assertEqual($myTemp->f2k(32), 273.15, "32 Fahrenheit is 273.15 Kelvin");
 	  $this->assertPattern('/error/i', $myTemp->f2k(-700), "-700 Fahrenheit should generate error message");	
 	}
 	function testFahrenheitToCelsius() {
@@ -40,17 +41,15 @@ class TestTemperature extends PHPUnit_Framework_TestCase {
     
 	function testKelvinToCelsius() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->k2c(100), -173, "100 Kelvin is =173 Celsius");
-	  $this->assertEqual($myTemp->k2c(10), -263, "10 Kelvin is -263 Celsius");
+	  $this->assertEqual($myTemp->k2c(100), -173.15, "100 Kelvin is -173.15 Celsius");
+	  $this->assertEqual($myTemp->k2c(10), -263.15, "10 Kelvin is -263.15 Celsius");
 	  $this->assertPattern('/error/i', $myTemp->k2c(-10), "-10 Kelvin to Celsius should generate error message");	
-      //$this->expectException($myTemp->k2c(), -263, "No parameter should throw exception");
-	}  
-      
+	}
 	function testKelvinToFahrenheit() {
 	  $myTemp= new Temperature();
-	  $this->assertEqual($myTemp->k2f(255), 0, "255 Kelvin is 0 Fahrenheit");
-	  $this->assertEqual($myTemp->k2f(0), -459, "0 Kelvin is -459 Fahrenheit");
+	  $this->assertEqual($myTemp->k2f(255), -0.67, "255 Kelvin is -0.67 Fahrenheit");
+	  $this->assertEqual($myTemp->k2f(0), -459.67, "0 Kelvin is -459.67 Fahrenheit");
 	  $this->assertPattern('/error/i', $myTemp->k2f(-10), "-10 Kelvin to Fahrenheit should generate error message");	
 	}
-	
+
 }
