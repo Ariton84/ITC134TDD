5435a41e5be9ea35fef81934a6409ebd5fd3eba3
Ariton84 <vanver@gmail.com>
2016-12-08 10:51:43 -0800

    Update temperature_test.php

--- temperature_test.php
+++ temperature_test.php
@@ -14,9 +14,9 @@ class TestTemperature extends PHPUnit_Framework_TestCase {
     }
 	function testCelsiusToKelvin() {
 	  $myTemp= new Temperature();
-	  assertEqual($myTemp->c2k(100), 373, "100 Celsius is 373 Kelvin");
-	  assertEqual($myTemp->c2k(0), 273, "0 Celsius is 373 Kelvin");
-	  assertPattern('/error/i', $myTemp->c2k(-274), "-274 Celsius should generate error message");	
+	  $this->assertEqual($myTemp->c2k(100), 373, "100 Celsius is 373 Kelvin");
+	  $this->assertEqual($myTemp->c2k(0), 273, "0 Celsius is 373 Kelvin");
+	  $this->assertPattern('/error/i', $myTemp->c2k(-274), "-274 Celsius should generate error message");	
 	}
 	function testCelsiusToFahrenheit() {
 	  $myTemp= new Temperature();
